bacb7353d2c4079dacc54ec4fee3ff8696819628
Jakub Zych <info@keios.eu>
2020-11-08 14:55:01 +0100

    Bearer Token support in request sender

--- classes/RequestSender.php
+++ classes/RequestSender.php
@@ -14,21 +14,64 @@ namespace Keios\Apparatus\Classes;
  */
 class RequestSender
 {
+    private $headers = [];
+    /**
+     * RequestSender constructor.
+     *
+     * @param string      $contentType
+     * @param string|null $bearerToken
+     */
+    public function __construct($bearerToken = null, $contentType = 'application/json')
+    {
+        $this->headers[] = 'Content-Type: '.$contentType;
+        if ($bearerToken) {
+            $this->headers[] = 'Authorization: Bearer '.$bearerToken;
+        }
+    }
+
     /**
      * @param array  $data
      * @param string $url
      * @return array|bool
      */
-    public function sendPostRequest(array $data, string $url)
+    public function sendPostRequest(array $data, string $url, bool $asJson = false)
     {
+        if($asJson){
+            $data = json_encode($data);
+        } else {
+            $data = http_build_query($data);
+        }
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         $result = curl_exec($ch);
 
+        curl_close($ch);
+
+        return $result;
+    }
+
+    public function sendPutRequest(array $data, $url, bool $asJson = false)
+    {
+        if($asJson){
+            $data = json_encode($data);
+        } else {
+            $data = http_build_query($data);
+        }
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+        $result = curl_exec($ch);
         curl_close($ch);
 
         return $result;
@@ -48,6 +91,11 @@ class RequestSender
             $url .= '?'.$query;
         }
         curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+        //curl_setopt($ch, CURLOPT_POSTFIELDS, $requestData);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         if($ignoreSsl){
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
