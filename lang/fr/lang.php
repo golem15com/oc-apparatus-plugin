4e46ce7000d01446ce77457fd1b386b5e96a4edb
Jakub Zych <jakub@keios.eu>
2024-03-18 10:06:49 +0100

    Translations

---
+++ lang/fr/lang.php
@@ -0,0 +1,58 @@
+<?php
+
+return [
+    'errors' => [
+        'pageWithComponentNotFound' => 'Le composant %s n\'est lié à aucune page dans le CMS.',
+        'parameterNotFound' => 'Le paramètre %s n\'a pas été trouvé dans la configuration du composant %s.',
+    ],
+    'jobs' => [
+        'menu_label' => 'Emplois',
+        'return_to_list' => 'Retour aux emplois',
+        'delete_confirm' => 'Voulez-vous vraiment supprimer cet emploi?',
+        'delete_selected_confirm' => 'Supprimer les emplois sélectionnés?',
+        'delete_selected_success' => 'Les emplois sélectionnés ont été supprimés avec succès.',
+        'delete_selected_empty' => 'Il n\'y a pas d\'emplois sélectionnés à supprimer.',
+        'in_queue' => 'En attente',
+        'in_progress' => 'En cours',
+        'complete' => 'Terminé',
+        'error' => 'Erreur',
+        'stopped' => 'Arrêté',
+        'unknown' => 'Inconnu',
+    ],
+    'job' => [
+        'label' => 'Emploi',
+        'create_title' => 'Créer un emploi',
+        'update_title' => 'Modifier l\'emploi',
+        'preview_title' => 'Aperçu de l\'emploi',
+        'list_title' => 'Gérer les emplois',
+    ],
+    'strings' => [
+        'inject_main' => 'Injecter le script principal',
+        'inject_main_desc' => 'Script principal pour afficher les messages sur les événements du framework',
+        'inject_noty' => 'Injecter Noty.js',
+        'inject_noty_desc' => 'Bibliothèque Noty.js pour afficher les notifications',
+        'inject_animate' => 'Injecter animate.css',
+        'inject_animate_desc' => 'Injecter les styles d\'animation CSS',
+    ],
+    'settings' => [
+        'messaging' => 'Notifications',
+    ],
+    'labels' => [
+        'knobFormWidget' => 'Knob FormWidget',
+        'kFwDescription' => 'Présente des entrées numériques sous forme de bouton',
+        'pluginName' => 'Processeur de scénario de logique métier',
+        'apparatus' => 'Appareil',
+        'jobs' => 'Emplois',
+    ],
+    'listtoggle' => [
+        'title_true' => 'Oui',
+        'title_false' => 'Non',
+        'text_true' => 'Activé',
+        'text_false' => 'Désactivé',
+    ],
+    'permissions' => [
+        'tab' => 'Appareil',
+        'access_settings' => 'Accès aux paramètres',
+        'access_jobs' => 'Accès aux emplois',
+    ],
+];
