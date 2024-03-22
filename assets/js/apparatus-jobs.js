da5c243968fc947d5913e1357e187b09defbfc83
Jakub Zych <jakub@keios.eu>
2024-03-22 09:37:23 +0100

    fix jobs auto update

--- assets/js/apparatus-jobs.js
+++ assets/js/apparatus-jobs.js
@@ -1,70 +1,69 @@
 $(function () {
 
-  function getAllVisibleJobsElems() {
-    return $('[data-job-status]');
-  }
-
-  function filterOnlyChangeableJobsElems(jobs) {
-    return jobs.filter(function () {
-      return $(this).data('job-status') < 2;
-    });
-  }
-
-  function mapJobsElemsToJobIds(jobs) {
-    return jobs.map(function () {
-      return $(this).data('job-id');
-    });
-  }
-
-  function setCurrentProgress(data) {
-    data.jobs.forEach(function (job) {
-      var $status = $('[data-job-id="' + job.id + '"]');
-      var $bar = $('[data-job-progress="' + job.id + '"]');
-      var $text = $('[data-job-progress-text="' + job.id + '"]');
-
-      $status.html(job.status);
-
-      if (job.percent == 100 || job.statusCode > 1) {
-        $bar.parent().remove();
-        $text.parent().remove();
-        $('#job-cancel-btn').remove();
-        $('#job-force-cancel-btn').remove();
-        return;
-      }
-
-      $bar.attr('aria-valuenow', job.progress_current);
-      $bar.attr('aria-valuemax', job.progress_max);
-      $bar.css('width', job.percent + '%');
-      $text.html(job.percent);
-    });
-  }
-
-  function refreshProgress() {
-    var allJobElements = getAllVisibleJobsElems();
-    var jobsRequiringUpdate = mapJobsElemsToJobIds(filterOnlyChangeableJobsElems(allJobElements)).toArray();
-
-    if (jobsRequiringUpdate.length > 0) {
-      $.request('onGetProgress', {data: {ids: jobsRequiringUpdate}, success: setCurrentProgress});
+    function getAllVisibleJobsElems() {
+        return $('[data-job-progress]');
     }
 
-    allJobElements.toArray().forEach(function (jobElem) {
-      if ($(jobElem).data('job-status') > 1) {
-        var id = $(jobElem).data('job-id');
-        var $bar = $('[data-job-progress="' + id + '"]');
-        var $text = $('[data-job-progress-text="' + id + '"]');
-        $bar.parent().remove();
-        $text.parent().remove();
-      }
-    });
+    function filterOnlyChangeableJobsElems(jobs) {
+        return jobs.filter(function () {
+            return $(this).data('job-status') < 2;
+        });
+    }
 
-    $('#progress-container').removeClass('hidden');
+    function mapJobsElemsToJobIds(jobs) {
+        return jobs.map(function () {
+            return $(this).data('job-id');
+        });
+    }
 
-    nextRefreshCycle();
-  }
+    function setCurrentProgress(data) {
+        data.jobs.forEach(function (job) {
+            var $status = $('[data-job-id="' + job.id + '"]');
+            var $bar = $('[data-job-progress="' + job.id + '"]');
+            var $text = $('[data-job-progress-text="' + job.id + '"]');
 
-  function nextRefreshCycle() {
-    setTimeout(refreshProgress, 1000);
-  }
+            //$status.html(job.status);
 
-  refreshProgress();
+            if (job.percent == 100 || job.statusCode > 1) {
+                $bar.parent().remove();
+                $text.parent().remove();
+                $('#job-cancel-btn').remove();
+                $('#job-force-cancel-btn').remove();
+                return;
+            }
+
+            $bar.attr('aria-valuenow', job.progress_current);
+            $bar.attr('aria-valuemax', job.progress_max);
+            $bar.css('width', job.percent + '%');
+            $text.html(job.percent);
+        });
+    }
+
+    function refreshProgress() {
+        var allJobElements = getAllVisibleJobsElems();
+        var jobsRequiringUpdate = mapJobsElemsToJobIds(filterOnlyChangeableJobsElems(allJobElements)).toArray();
+        if (jobsRequiringUpdate.length > 0) {
+            $.request('onGetProgress', {data: {ids: jobsRequiringUpdate}, success: setCurrentProgress});
+        }
+
+        allJobElements.toArray().forEach(function (jobElem) {
+            if ($(jobElem).data('job-status') > 1) {
+                var id = $(jobElem).data('job-id');
+                var $bar = $('[data-job-progress="' + id + '"]');
+                var $text = $('[data-job-progress-text="' + id + '"]');
+   //             $bar.parent().remove();
+   //             $text.parent().remove();
+            }
+        });
+
+        $('#progress-container').removeClass('hidden');
+
+        nextRefreshCycle();
+    }
+
+    function nextRefreshCycle() {
+        setTimeout(refreshProgress, 2000);
+    }
+
+    refreshProgress();
 });
